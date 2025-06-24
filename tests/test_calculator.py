ba2b4e3b89c3a1b5040f4c441d2d711a0ec00566
Viraj Rajurkar <virajrajurkar@gmail.com>
2025-06-24 15:15:08 +0200

    Fixed Unit test to use calculator instance

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,17 +1,19 @@
 import unittest
-
 from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_add(self):
-        self.assertEqual(Calculator.calculate("add", 1, 1), 2)
+        self.assertEqual(self.calc.calculate("add", 1, 1), 2)
 
     def test_sub(self):
-        self.assertEqual(Calculator.calculate("subtract", 2, 1), 1)
+        self.assertEqual(self.calc.calculate("subtract", 2, 1), 1)
 
     def test_invalidOperation(self):
-        self.assertEqual(Calculator.calculate("invalid operation", 2, 1), "Error: Invalid operation")
+        self.assertEqual(self.calc.calculate("invalid operation", 2, 1), "Error: Invalid operation")
     
 if __name__ == "__main__":
     unittest.main()
